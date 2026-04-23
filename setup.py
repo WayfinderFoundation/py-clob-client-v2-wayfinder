4a09d38f15ed38bf2e8aa89b033c7fe6a42d462b
Leejoosub <joosublee@hotmail.com>
2026-04-23 12:14:40 -0400

    update package name

--- setup.py
+++ setup.py
@@ -5,16 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py_clob_client_v2",
+    name="py_clob_client_v2_wayfinder",
     version="1.0.0",
-    author="Polymarket Engineering",
-    author_email="engineering@polymarket.com",
-    maintainer="Polymarket Engineering",
-    maintainer_email="engineering@polymarket.com",
-    description="Python client for the Polymarket CLOBV2",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Polymarket/py-clob-client-v2",
+    author="Wayfinder",
     install_requires=[
         "eth-account>=0.13.0",
         "eth-utils>=4.1.1",
@@ -22,9 +15,6 @@ setuptools.setup(
         "py-order-utils>=0.3.2",
         "httpx[http2]>=0.27.0",
     ],
-    project_urls={
-        "Bug Tracker": "https://github.com/Polymarket/py-clob-client-v2/issues",
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
